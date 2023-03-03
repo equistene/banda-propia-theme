c6322e73fe66c3b4f9518f0ba8e3925e4f20d993
Cristian Berrios <cristian@ilogica.cl>
2023-03-03 17:56:52 -0300

    home books

--- index.php
+++ index.php
@@ -42,39 +42,36 @@
 				<?php
 				$args = array(
 					'post_type' => 'product',
-					'posts_per_page' => 4
+					'posts_per_page' => 6
 				);
 				$query = new WP_Query($args);
 
 				if($query->have_posts()) : while($query->have_posts()): $query->the_post(); ?>
-
-					<div class="itemsCatalogo">
-						
-							<div class="itemBook">
-								<a href="<?php the_permalink(); ?>">
-									<div class="thumb">
-										<?php the_post_thumbnail('large'); ?>
-									</div>
-									<div class="data">
-										<h2><?php the_title(); ?></h2>
-										<h3>
-											<?php $termAutor = wp_get_object_terms($post->ID, 'autor'); ?>
-											<?php foreach($termAutor as $key=>$value) { $nameAutor = $termAutor[$key]->name; echo $nameAutor . " • "; } ?>
-										</h3>
-										<p>
-											<?php
-												$term_list = wp_get_object_terms($post->ID, 'coleccion');
-												$termName = $term_list[0]->name;              
-												echo $termName;
-											?>
-										</p>
-										<?php if ($price_html = get_post_meta(get_the_ID(), '_regular_price', true)) : ?>
-											<span class="price">$<?php echo $price_html; ?></span>
-										<?php endif; ?>
-									</div>
-								</a>
-							</div>  
-					</div>
+					
+					<div class="itemBook">
+						<a href="<?php the_permalink(); ?>">
+							<div class="thumb">
+								<?php the_post_thumbnail('large'); ?>
+							</div>
+							<div class="data">
+								<h2><?php the_title(); ?></h2>
+								<h3>
+									<?php $termAutor = wp_get_object_terms($post->ID, 'autor'); ?>
+									<?php foreach($termAutor as $key=>$value) { $nameAutor = $termAutor[$key]->name; echo $nameAutor . " • "; } ?>
+								</h3>
+								<p>
+									<?php
+										$term_list = wp_get_object_terms($post->ID, 'coleccion');
+										$termName = $term_list[0]->name;              
+										echo $termName;
+									?>
+								</p>
+								<?php if ($price_html = get_post_meta(get_the_ID(), '_regular_price', true)) : ?>
+									<span class="price">$<?php echo $price_html; ?></span>
+								<?php endif; ?>
+							</div>
+						</a>
+					</div>  					
 
 				<?php endwhile; wp_reset_postdata(); endif; ?>				
 				</div>
