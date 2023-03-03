ee65b4b01310c63ae50c8e1d6bbe787234b86f42
Cristian Berrios <cristian@ilogica.cl>
2023-03-03 16:41:04 -0300

    loop books

--- loop-books.php
+++ loop-books.php
@@ -1,28 +1,28 @@
-<div class="itemsCatalogo">      
+<div class="itemsCatalogo">
   <?php if (have_posts()): while (have_posts()) : the_post(); ?>
     <div class="itemBook">
       <a href="<?php the_permalink(); ?>">
-        <div class="thumb">        
-          <?php the_post_thumbnail('large'); ?>          
+        <div class="thumb">
+          <?php the_post_thumbnail('large'); ?>
         </div>
         <div class="data">
           <h2><?php the_title(); ?></h2>
           <h3>
-            <?php $termAutor = wp_get_object_terms( $post->ID, 'autor'); ?>
+            <?php $termAutor = wp_get_object_terms($post->ID, 'autor'); ?>
             <?php foreach($termAutor as $key=>$value) { $nameAutor = $termAutor[$key]->name; echo $nameAutor . " • "; } ?>
           </h3>
           <p>
             <?php
-              $term_list = wp_get_object_terms( $post->ID, 'coleccion');
+              $term_list = wp_get_object_terms($post->ID, 'coleccion');
               $termName = $term_list[0]->name;              
               echo $termName;
             ?>
-
-			  <?php if ( $price_html = $product->get_price_html() ) : ?>
-	<span class="price"><?php echo $price_html; ?></span>
-			</p>
-    </div>    <?php endwhile; endif; ?>
-
+          </p>
+          <?php if ($price_html = get_post_meta(get_the_ID(), '_regular_price', true)) : ?>
+            <span class="price"><?php echo $price_html; ?></span>
+          <?php endif; ?>
+        </div>
+      </a>
+    </div>
+  <?php endwhile; endif; ?>
 </div>
-
-</div>  
