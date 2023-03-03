c7cab816520d62c85522f0a3568ab84a31725205
Cristian Berrios <cristian@ilogica.cl>
2023-03-03 17:54:50 -0300

    home books

--- index.php
+++ index.php
@@ -48,7 +48,33 @@
 
 				if($query->have_posts()) : while($query->have_posts()): $query->the_post(); ?>
 
-					<?php get_template_part('template-parts/parts/item'); ?>
+					<div class="itemsCatalogo">
+						
+							<div class="itemBook">
+								<a href="<?php the_permalink(); ?>">
+									<div class="thumb">
+										<?php the_post_thumbnail('large'); ?>
+									</div>
+									<div class="data">
+										<h2><?php the_title(); ?></h2>
+										<h3>
+											<?php $termAutor = wp_get_object_terms($post->ID, 'autor'); ?>
+											<?php foreach($termAutor as $key=>$value) { $nameAutor = $termAutor[$key]->name; echo $nameAutor . " • "; } ?>
+										</h3>
+										<p>
+											<?php
+												$term_list = wp_get_object_terms($post->ID, 'coleccion');
+												$termName = $term_list[0]->name;              
+												echo $termName;
+											?>
+										</p>
+										<?php if ($price_html = get_post_meta(get_the_ID(), '_regular_price', true)) : ?>
+											<span class="price">$<?php echo $price_html; ?></span>
+										<?php endif; ?>
+									</div>
+								</a>
+							</div>  
+					</div>
 
 				<?php endwhile; wp_reset_postdata(); endif; ?>				
 				</div>
