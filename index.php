23dcce132fde5af2f6b6205251f237cfefe3c0a2
Cristian Berrios <cristian@ilogica.cl>
2023-03-03 17:47:35 -0300

    test book home

--- index.php
+++ index.php
@@ -37,6 +37,21 @@
 				<?php endwhile; wp_reset_postdata(); endif; ?>				
 		</section>
 
+		<section class="posts-home container">						
+				<?php
+				$args = array(
+					'post_type' => 'product',
+					'post_per_page' => 8
+				);
+				$query = new WP_Query($args);
+
+				if($query->have_posts()) : while($query->have_posts()): $query->the_post(); ?>
+
+					<?php get_template_part('template-parts/parts/item'); ?>
+
+				<?php endwhile; wp_reset_postdata(); endif; ?>				
+		</section>
+
 	</main>
 
 <?php get_footer(); ?>
